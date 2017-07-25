89e8e5e487e15f29fef94ae61e8e1606b5034c54
Ubuntu <ubuntu@ip-172-31-11-40.us-west-2.compute.internal>
2017-07-25 22:51:31 +0000

    Initial Commit

---
+++ housing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Script to automate my housing search
+curl --silent https://www.capitolhillhousing.org/findyourapartment/ > $temp_file.html
+NewUpdate=$(lynx  -dump $temp_file.html | grep update)
+LastUpdate=$NewUpdate
+MinuteCount=0
+
+while true; do
+
+	while true; do
+	
+		let "MinuteCount+=1"
+		
+		if [ "$MinuteCount==1440" ]
+		then
+			echo "Running" | mail "4252447077@sms.myboostmobile.com"
+			MinuteCount=0
+		fi
+        	
+		curl --silent https://www.capitolhillhousing.org/findyourapartment/ > $temp_file.html
+        	Newupdate=$(lynx  -dump $temp_file.html | grep update)
+        	if [ "$NewUpdate" != "$LastUpdate" ] 
+		then
+			break
+		fi
+
+		echo "in search loop"
+        	sleep 60
+	done
+
+	$LastUpdate=$NewUpdate
+
+
+	#Search for builings that are in the %50 median income bracket and sent an sms
+	lynx  -dump $temp_file.html | grep -B 3 50 | mail "4252447077@sms.myboostmobile.com"
+	echo "Mail Time"
+
+done
